eee1e06d2704e26a755b20115f85f1572b5a1775
jmshea <jmshea@users.noreply.github.com>
2024-03-01 16:48:49 +0000

    deploy: 5b64863b1bbb87d3ecf57592e9dd4baeb8ee99e7

---
+++ _static/structured_data.js
@@ -0,0 +1,17 @@
+<script type="application/ld+json">
+{
+"@context" : "https://schema.org",
+"@type" : "WebSite",
+"name" : "Foundations of Data Science with Python",
+"alternateName" : "FDSP",
+"url" : "https://www.fdsp.net/",
+"potentialAction": {
+"@type": "SearchAction",
+"target": {
+  "@type": "EntryPoint",
+  "urlTemplate": "https://www.fdsp.net/search.html?q={search_term_string}"
+},
+"query-input": "required name=search_term_string"
+}
+}
+</script>
